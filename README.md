9bc891c07c38c0396351316afc831118c76ca6d6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-12-30 22:57:16 +0800

    Update

---
+++ README.md
@@ -0,0 +1,14 @@
+# Webpack starter kit
+
+> Baseline configuration for Webpack
+
+## Features
+
+- JavaScript via Babel
+- CSS with [cssnext]
+- HTML
+- Images
+- Webpack dev server
+- LiveReload
+
+[cssnext]: http://cssnext.io/
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-const div = document.createElement('div')
-div.innerHTML = 'Now go and make something great!'
-div.className = 'webpack-message'
-
-document.body.append(div)
+setTimeout(() => {
+  document.querySelector('.webpack-welcome').className += ' -show'
+}, 100)
