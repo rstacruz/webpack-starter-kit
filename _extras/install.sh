8d8d9be1fd3f8cf39f8e55f8f3bc2bceb70e8acb
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-03 15:12:42 +0800

    Fix install.sh

--- _extras/install.sh
+++ _extras/install.sh
@@ -8,7 +8,8 @@ fi
 curl -sSL https://github.com/rstacruz/webpack-starter-kit/archive/master.tar.gz \
  | tar zxv --strip-components=1 \
  --exclude='README.md' \
- --exclude='_extras'
+ --exclude='_extras' \
+ --exclude='EXTRAS.md'
 
 mv _README.md README.md
 yarn
