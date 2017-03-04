acb9d96b3ea6f23c8230472a4e8a56c15ea7ed61
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-03-04 15:01:31 +0800

    Update

---
+++ _extras/simple_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if ! which yarn >/dev/null; then
+  echo "! You need yarn. See: http://yarnpkg.com/"
+  exit 1
+fi
+
+curl -sSL https://github.com/rstacruz/webpack-starter-kit/archive/master.tar.gz \
+ | tar zxv --strip-components=1 \
+ --exclude='README.md' \
+ --exclude='_extras' \
+ --exclude='EXTRAS.md' \
+ --exclude='HISTORY.md' \
+
+mv _README.md README.md
+yarn
