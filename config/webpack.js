f39482503a33f038982c5a2b917c14a14bc48a2b
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-09-19 18:39:32 +0800

    Update

--- config/webpack.js
+++ config/webpack.js
@@ -1,13 +1,20 @@
+const dirname = require('path').dirname
+const basename = require('path').basename
+
 module.exports = function getConfig (opts) {
-  const { root: ROOT, source: SRC, destination: DEST, entries: ENTRIES, copy: COPY } = opts
+  const { from: FROM, to: TO, copy: COPY } = opts
+  const ROOT = dirname(module.parent.id)
   const webpack = require('webpack')
   const resolve = require('path').resolve
   const ExtractTextPlugin = require('extract-text-webpack-plugin')
   const CopyWebpackPlugin = require('copy-webpack-plugin')
-  const CleanWebpackPlugin = require('clean-webpack-plugin')
   const LiveReloadPlugin = require('webpack-livereload-plugin')
 
   const DEBUG = process.env.NODE_ENV !== 'production'
+  const DEST = dirname(TO)
+  const ENTRIES = {
+    [basename(TO).replace(/\.js$/, '')]: [FROM]
+  }
 
   const config = {
     cache: true,
@@ -69,9 +76,6 @@ module.exports = function getConfig (opts) {
     },
 
     plugins: [
-      // Delete old files when compiling
-      new CleanWebpackPlugin([ DEST ]),
-
       // Extract to .css
       new ExtractTextPlugin({
         filename: '[name].css',
@@ -82,10 +86,15 @@ module.exports = function getConfig (opts) {
       new webpack.EnvironmentPlugin({
         NODE_ENV: 'development'
       }),
-
+    ].concat(COPY ? [
       // Copying files directly
-      new CopyWebpackPlugin(COPY)
-    ],
+      new CopyWebpackPlugin(COPY.map(({ from, to }) => {
+        return {
+          from: resolve(ROOT, from),
+          to: resolve(ROOT, to)
+        }
+      }))
+    ] : []),
 
     // Hide source maps in production (no sourceMappingURL)
     devtool: DEBUG ? 'source-map' : 'hidden-source-map',
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-module.exports = require('./config/webpack')({
-  root: __dirname,
-  destination: './public',
+module.exports = [
+  require('./config/webpack')({
+    to: './public/assets/js/app.js',
+    from: './web/js/app.js',
 
-  entries: {
-    'assets/js/app': './web/js/app.js', // JS
-    'assets/css/app': './web/css/app.js' // CSS
-  },
-
-  copy: [
-    { from: 'web/assets', to: 'assets' },
-    { from: 'web/html', to: '.' }
-  ]
-})
+    copy: [
+      { from: 'web/assets', to: 'public/assets' },
+      { from: 'web/html', to: 'public' }
+    ]
+  }),
+  require('./config/webpack')({
+    to: './public/assets/css/app.js',
+    from: './web/css/app.js',
+  })
+]
