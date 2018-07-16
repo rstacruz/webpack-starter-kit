d8da52b68a22e670513b742a5a726c45bd8b48b8
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-07-16 17:10:58 +0800

    Functionize the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,102 +10,104 @@ const SRC = './web'
 const DEST = './public'
 const POLYFILLS = DEBUG ? [] : ['babel-polyfill', 'es6-promise/auto']
 
-module.exports = {
-  context: __dirname,
-  entry: {
-    'assets/js/app': [...POLYFILLS, `${SRC}/js/app.js`],
-    'assets/css/app': [...POLYFILLS, `${SRC}/css/app.js`]
-  },
+module.exports = () => {
+  return {
+    context: __dirname,
+    entry: {
+      'assets/js/app': [...POLYFILLS, `${SRC}/js/app.js`],
+      'assets/css/app': [...POLYFILLS, `${SRC}/css/app.js`]
+    },
 
-  output: {
-    path: resolve(__dirname, DEST),
-    filename: '[name].js',
-    pathinfo: !!DEBUG,
-    devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]'
-  },
+    output: {
+      path: resolve(__dirname, DEST),
+      filename: '[name].js',
+      pathinfo: !!DEBUG,
+      devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]'
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          {
-            loader: 'css-loader',
-            options: DEBUG
-              ? {
-                  url: false,
-                  sourceMap: true,
-                  importLoaders: 1
-                }
-              : {
-                  url: false
-                }
-          },
-          {
-            loader: 'postcss-loader',
-            options: DEBUG ? { sourceMap: 'inline' } : {}
-          }
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env'],
-              cacheDirectory: true
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            {
+              loader: 'css-loader',
+              options: DEBUG
+                ? {
+                    url: false,
+                    sourceMap: true,
+                    importLoaders: 1
+                  }
+                : {
+                    url: false
+                  }
+            },
+            {
+              loader: 'postcss-loader',
+              options: DEBUG ? { sourceMap: 'inline' } : {}
             }
-          }
-        ]
-      }
-    ]
-  },
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env'],
+                cacheDirectory: true
+              }
+            }
+          ]
+        }
+      ]
+    },
 
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
 
-  plugins: [
-    // Delete old files when compiling
-    new CleanWebpackPlugin([DEST]),
+    plugins: [
+      // Delete old files when compiling
+      new CleanWebpackPlugin([DEST]),
 
-    // Extract to .css
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
+      // Extract to .css
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      }),
 
-    // Compress React (and others)
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development'
-    }),
+      // Compress React (and others)
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: 'development'
+      }),
 
-    // Copying files directly
-    new CopyWebpackPlugin([
-      { from: `${SRC}/assets`, to: './assets' },
-      { from: `${SRC}/html`, to: '.' }
-    ])
-  ].concat(
-    DEBUG
-      ? [
-          // LiveReload in development
-          new LiveReloadPlugin({
-            appendScriptTag: true
-          }),
+      // Copying files directly
+      new CopyWebpackPlugin([
+        { from: `${SRC}/assets`, to: './assets' },
+        { from: `${SRC}/html`, to: '.' }
+      ])
+    ].concat(
+      DEBUG
+        ? [
+            // LiveReload in development
+            new LiveReloadPlugin({
+              appendScriptTag: true
+            }),
 
-          // Debug mode for old webpack plugins
-          new webpack.LoaderOptionsPlugin({
-            debug: true
-          })
-        ]
-      : []
-  ),
+            // Debug mode for old webpack plugins
+            new webpack.LoaderOptionsPlugin({
+              debug: true
+            })
+          ]
+        : []
+    ),
 
-  // Hide source maps in production (no sourceMappingURL)
-  devtool: DEBUG ? 'source-map' : 'hidden-source-map',
+    // Hide source maps in production (no sourceMappingURL)
+    devtool: DEBUG ? 'source-map' : 'hidden-source-map',
 
-  stats: 'minimal'
+    stats: 'minimal'
+  }
 }
